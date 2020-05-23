38776202dbaddbc0a3d9ee5421df6165e0fbd6c9
VladanR <x3m.rocker.035@gmail.com>
2020-05-23 23:21:16 +0200

    Initialize project

---
+++ ShakeAndBake/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ShakeAndBake
+//
+//  Created by Vladan Randjelovic on 23/05/2020.
+//  Copyright © 2020 Vladan Randjelovic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
